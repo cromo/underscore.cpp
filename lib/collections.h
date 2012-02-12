8f2bd8a84d79e17e85b51293bb7facbc1116074a
Cristián Romo <cristian.g.romo@gmail.com>
2012-02-12 01:03:33 -0600

    Added to_array.

--- lib/collections.h
+++ lib/collections.h
@@ -156,6 +156,22 @@ typename Container::iterator min(Container container) {
 // sorted_index
 // shuffle
 // to_array
+template<typename Container>
+typename Container::value_type* to_array(Container container) {
+  typename Container::value_type* array =
+      new typename Container::value_type[container.size()];
+  struct {
+    int numeric;
+    typename Container::iterator iterator;
+  } i;
+  for (i.numeric = 0, i.iterator = container.begin();
+      i.iterator != container.end();
+      ++i.numeric, ++i.iterator) {
+    array[i.numeric] = *i.iterator;
+  }
+
+  return array;
+}
 
 // size
 template<typename Container>
