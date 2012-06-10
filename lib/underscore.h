cdf47a91147cc4e67fca88990d60bf8e426c4eab
Cristián Romo <cristian.g.romo@gmail.com>
2012-06-10 13:41:09 -0500

    Added last.

--- lib/underscore.h
+++ lib/underscore.h
@@ -390,6 +390,20 @@ ResultContainer initial(Container& container, int n) {
 }
 
 // last
+template<typename Container>
+typename Container::iterator last(Container& container) {
+  typename Container::iterator last = container.begin();
+  std::advance(last, container.size() - 1);
+  return last;
+}
+
+template<typename ResultContainer, typename Container>
+ResultContainer last(Container& container, int n) {
+  typename Container::iterator begin = container.begin();
+  std::advance(begin, container.size() - n);
+  return ResultContainer(begin, container.end());
+}
+
 // rest/tail
 
 // compact
