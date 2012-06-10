c1cf106886140e98744e5995f4cdc36c8f21f380
Cristián Romo <cristian.g.romo@gmail.com>
2012-06-10 13:26:22 -0500

    Added initial.
    Something odd happened with all the lines in the file - I think the line
    endings changed, so I added the recommended .gitattributes to fix it. It
    doesn't seem like it has, so I manually converted the line endings to
    UNIX style.

--- lib/underscore.h
+++ lib/underscore.h
@@ -374,6 +374,20 @@ ResultContainer head(Container& container, int count) {
 }
 
 // initial
+template<typename ResultContainer, typename Container>
+ResultContainer initial(Container& container) {
+  typename Container::iterator end = container.begin();
+  std::advance(end, container.size() - 1);
+  return ResultContainer(container.begin(), end);
+}
+
+template<typename ResultContainer, typename Container>
+ResultContainer initial(Container& container, int n) {
+  typename Container::iterator end = container.begin();
+  std::advance(end, container.size() - n);
+  return ResultContainer(container.begin(), end);
+}
+
 // last
 // rest/tail
 
