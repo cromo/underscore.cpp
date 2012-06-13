1eb5d2b51345a0aa9e79a55ddd9324e7699982f0
Cristián Romo <cristian.g.romo@gmail.com>
2012-06-13 17:02:25 -0500

    Added flatten.

--- lib/underscore.h
+++ lib/underscore.h
@@ -625,7 +625,66 @@ ResultContainer flatten_one_layer(Container const& container) {
   }
   return result;
 }
+
+template<typename T>
+class HasConstIterator {
+ private:
+  typedef char yes[1];
+  typedef char no[2];
+  template<typename C>
+  static yes& test(typename C::const_iterator*);
+  template<typename C>
+  static no& test(...);
+ public:
+  static bool const value = sizeof(test<T>(0)) == sizeof(yes);
+};
+
+template<typename ResultContainer, typename Container>
+typename enable_if<
+    !HasConstIterator<typename Container::value_type>::value,
+    void>::type flatten_loop(
+    ResultContainer& result,
+    Container const& container) {
+  for (typename Container::const_iterator i = container.begin();
+      i != container.end();
+      ++i) {
+    add_to_container(result, *i);
+  }
+}
+
+template<typename ResultContainer, typename Container>
+typename enable_if<
+    HasConstIterator<typename Container::value_type>::value,
+    void>::type flatten_loop(
+    ResultContainer& result,
+    Container const& container) {
+  for (typename Container::const_iterator i = container.begin();
+      i != container.end();
+      ++i) {
+    flatten_loop(result, *i);
+  }
+}
+
 }  // namespace helper
+
+template<typename ResultContainer, typename Container>
+ResultContainer flatten(Container const& container) {
+  ResultContainer result;
+  helper::flatten_loop(result, container);
+  return result;
+}
+
+template<typename ResultContainer, bool shallow, typename Container>
+typename helper::enable_if<shallow == true, ResultContainer>::type flatten(
+    Container const& container) {
+  return helper::flatten_one_layer<ResultContainer>(container);
+}
+
+template<typename ResultContainer, bool shallow, typename Container>
+typename helper::enable_if<shallow == false, ResultContainer>::type flatten(
+    Container const& container) {
+  return flatten<ResultContainer>(container);
+}
 
 // without
 // union
